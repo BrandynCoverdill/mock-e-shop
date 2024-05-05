55c3460a83147baf1a05a330bc4ef64351dc8c55
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-05-05 00:32:29 -0600

    Fetches products and displays on page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,68 @@
-import { Typography, Box } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import {
+	Typography,
+	Box,
+	Card,
+	CardContent,
+	CardHeader,
+	CardMedia,
+	CardActions,
+	IconButton,
+} from '@mui/material';
 
-export default function ProductCard() {
+export default function ProductCard({ product }) {
 	return (
 		<Box>
-			<Typography>Product Card</Typography>
+			<Card
+				sx={{
+					// maxWidth: '400px',
+					display: 'flex',
+				}}
+				raised
+			>
+				{/* Product Image */}
+				<Box
+					sx={{
+						width: '100%',
+						margin: 'auto',
+					}}
+					p={1}
+				>
+					<CardMedia
+						component='img'
+						image={product.image}
+						title={product.title}
+						sx={{
+							objectFit: 'contain',
+							height: '100%',
+							width: '100%',
+						}}
+					/>
+				</Box>
+				{/* Product Content */}
+				<Box
+					sx={{
+						width: '100%',
+					}}
+				>
+					<CardContent>
+						<Typography variant='h5'>{product.title}</Typography>
+						<Typography variant='h6'>${product.price}</Typography>
+						{/* Actions for products */}
+						<CardActions>
+							<IconButton
+								sx={{
+									border: '1px solid black',
+								}}
+								disableTouchRipple
+							>
+								<Add />
+							</IconButton>
+						</CardActions>
+						<Typography variant='body2'>{product.description}</Typography>
+					</CardContent>
+				</Box>
+			</Card>
 		</Box>
 	);
 }
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,9 +1,38 @@
-import { Container, Typography } from '@mui/material';
+import { Card, Container, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import ProductCard from '../components/ProductCard';
+import Masonry from '@mui/lab/Masonry';
 
 export default function Store() {
+	const [products, setProducts] = useState([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const res = await fetch('https://fakestoreapi.com/products');
+			const json = await res.json();
+			setProducts(json);
+		};
+		fetchData();
+	}, []);
+
 	return (
 		<Container>
-			<Typography>Store</Typography>
+			<Masonry
+				spacing={3}
+				columns={{
+					xs: 1,
+					md: 2,
+				}}
+				sx={{
+					my: 1,
+					margin: 'auto',
+				}}
+			>
+				{products.map((product) => (
+					// @ts-ignore
+					<ProductCard product={product} />
+				))}
+			</Masonry>
 		</Container>
 	);
 }
