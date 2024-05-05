40b09441bccee17e4e07137c0f020c784aad4514
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-05-04 22:33:46 -0600

    created banner for homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,60 @@
 // The homepage of the app
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Button, Container, Paper, Typography } from '@mui/material';
+// @ts-ignore
+import storeProductsImg from '../../public/storeProducts.jpg';
+import { useNavigate } from 'react-router-dom';
+import { blue, green, grey } from '@mui/material/colors';
 
 export default function Home() {
+	const navigate = useNavigate();
 	return (
 		<Container>
-			<Box>
-				<Typography>Home</Typography>
+			<Box my={1}>
+				<Paper
+					elevation={5}
+					sx={{
+						width: '100%',
+						textAlign: 'center',
+						display: 'flex',
+						flexDirection: 'column',
+						backgroundImage: `url(${storeProductsImg})`,
+						height: '150px',
+						backgroundRepeat: 'no-repeat',
+						backgroundSize: 'cover',
+						backgroundPosition: 'center',
+						py: 1,
+					}}
+				>
+					{/* TODO: change font family */}
+					<Typography
+						sx={{
+							textWrap: 'noWrap',
+							flexGrow: '1',
+							textShadow: `5px 5px 5px ${grey[900]}`,
+							color: grey[100],
+						}}
+						variant='h3'
+						fontWeight='bold'
+						letterSpacing={2}
+					>
+						Mock eShop
+					</Typography>
+					<Button
+						onClick={() => navigate('store')}
+						variant='outlined'
+						sx={{
+							width: '50%',
+							margin: 'auto',
+							bgcolor: 'white',
+							'&:hover': {
+								bgcolor: blue[100],
+							},
+						}}
+						disableTouchRipple
+					>
+						View Products
+					</Button>
+				</Paper>
 			</Box>
 		</Container>
 	);
