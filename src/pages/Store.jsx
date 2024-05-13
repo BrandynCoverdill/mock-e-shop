4b4d91bad60824fc2e50357fb16b606522e66b4b
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-05-13 00:28:50 -0600

    Fixed bug where removing a single qty would remove entire product from cart.

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -52,12 +52,10 @@ export default function Store() {
 	// Removes a product from the cart
 	const handleRemoveProductFromCart = (productId) => {
 		const productToUpdate = cart.filter((product) => product.id === productId);
-		// BUG: Bug where when removing a single qty from a product, it removes all of it.
-		console.log(productToUpdate);
 		// If the quantity for the product is more than 1
-		if (productToUpdate.qty > 1) {
+		if (productToUpdate[0].qty > 1) {
 			const updatedCart = cart.map((product) => {
-				if (productId === product.id) {
+				if (productToUpdate[0].id === product.id) {
 					return { ...product, qty: (product.qty -= 1) };
 				} else {
 					return { ...product };
@@ -69,11 +67,13 @@ export default function Store() {
 			const updatedCart = cart.filter((product) => product.id !== productId);
 			setCart(updatedCart);
 		}
-		console.log(cart);
 	};
 
 	return (
 		<Container>
+			{cart.map((product) => (
+				<Typography>{product.id + ' = ' + product.qty}</Typography>
+			))}
 			<Masonry
 				spacing={3}
 				columns={{
