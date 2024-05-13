b4c8f913fec12db42879a056054f58ee6232f0fd
Brandyn Coverdill <brandyncoverdill98@gmail.com>
2024-05-13 03:59:21 -0600

    Fixed bug where the both arrays in cart.jsx would merge, causing a single product to show up twice.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,26 +4,35 @@ import {
 	CardContent,
 	CardMedia,
 	Container,
+	Grid,
 	Typography,
 } from '@mui/material';
 import { Context } from '../App';
 import { useContext } from 'react';
 import ProductCard from '../components/ProductCard';
+import { Masonry } from '@mui/lab';
 
-export default function Cart(
+export default function Cart({
 	handleAddProductToCart,
 	handleRemoveProductQtyFromCart,
 	handleQtyChange,
-	handleRemoveProductFromCart
-) {
+	handleRemoveProductFromCart,
+}) {
 	const { productsContext, cartContext } = useContext(Context);
 	const [products, setProducts] = productsContext;
 	const [cart, setCart] = cartContext;
 
-	const productCart = cart.concat(
-		products.filter((product) =>
-			cart.some((cartProduct) => cartProduct.id === product.id)
-		)
+	const productCart = [];
+
+	products.map((product) =>
+		cart.map((cartProduct) => {
+			if (cartProduct.id === product.id) {
+				productCart.push({
+					...product,
+					qty: cartProduct.qty,
+				});
+			}
+		})
 	);
 
 	return (
@@ -32,9 +41,28 @@ export default function Cart(
 				Cart and Checkout
 			</Typography>
 			<Box>
-				{productCart.map((product) => (
-					<Typography>{product.title}</Typography>
-				))}
+				<Masonry
+					spacing={3}
+					columns={{
+						xs: 1,
+						md: 2,
+					}}
+					sx={{
+						my: 1,
+						margin: 'auto',
+					}}
+				>
+					{productCart.map((product) => (
+						<ProductCard
+							product={product}
+							cart={cart}
+							handleAddProductToCart={handleAddProductToCart}
+							handleRemoveProductQtyFromCart={handleRemoveProductQtyFromCart}
+							handleQtyChange={handleQtyChange}
+							handleRemoveProductFromCart={handleRemoveProductFromCart}
+						/>
+					))}
+				</Masonry>
 			</Box>
 		</Container>
 	);
